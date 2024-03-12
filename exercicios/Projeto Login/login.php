1ec9e41fc1e05214923218e74aea1e9279f8e965
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2024-03-12 12:00:23 -0300

    mais atualizações

--- exercicios/Projeto Login/login.php
+++ exercicios/Projeto Login/login.php
@@ -1,8 +1,33 @@
+
 <?php
 
+#Para verificar sem os dados foram enviados do formulario para as variaveis
 
 
-echo "Deus é muuito bom!!!!!!!";
+
+// Verifica se o formulário foi enviado antes de acessar os dados
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verifica se o campo "email" foi enviado
+    if (isset($_POST["email1"])) {
+        $email = $_POST["email1"];
+    } else {
+        $email = "Campo de email não preenchido";
+    }
+
+    // Verifica se o campo "senha" foi enviado
+    if (isset($_POST["senha1"])) {
+        $senha = $_POST["senha1"];
+    } else {
+        $senha = "Campo de senha não preenchido";
+    }
+
+    // Exibe os valores de $email e $senha
+    echo "E-mail: $email <br> Senha: $senha ";
+} else {
+    echo "Formulário não enviado";
+}
+
+
 
 
 
